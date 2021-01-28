f50e68a7a1f0f1ad024ce5c1ebead5920919fc0e
The Prof <52168158+computationalknowledge@users.noreply.github.com>
2021-01-28 14:55:10 -0500

    Update LA_Starter1.java

--- LA_Starter1.java
+++ LA_Starter1.java
@@ -1,5 +1,22 @@
-public class MyFirstJavaProgram {
-      public static void main(String []args) 
-      { System.out.println("Hello World");
-  }
+
+public class LearningActivity1 {
+
+	public static void main(String[] args) {
+		Calculator c = new Calculator();
+		System.out.println(c.Add(2,2));
+	}
+
+}
+
+
+class Calculator{
+	
+	private float result;
+	
+	public float Add(float a, float b) {
+		result = a + b;
+		return result;
+		
+	}
+	
 }
