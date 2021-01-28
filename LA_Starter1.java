fdd07d482502e72a373a8c8f9253883192dd70d8
The Prof <52168158+computationalknowledge@users.noreply.github.com>
2021-01-28 15:31:40 -0500

    Update LA_Starter1.java

--- LA_Starter1.java
+++ LA_Starter1.java
@@ -3,7 +3,7 @@ public class LearningActivity1 {
 
 	public static void main(String[] args) {
 		Calculator c = new Calculator();
-		System.out.println(c.Add(2,2));
+		System.out.println(c.Divide(2,2));
 	}
 
 }
@@ -15,6 +15,24 @@ class Calculator{
 	
 	public float Add(float a, float b) {
 		result = a + b;
+		return result;
+		
+	}	
+	
+	public float Substract(float a, float b) {
+		result = a - b;
+		return result;
+		
+	}	
+	
+	public float Multipy(float a, float b) {
+		result = a * b;
+		return result;
+		
+	}	
+	
+	public float Divide(float a, float b) {
+		result = a / b;
 		return result;
 		
 	}
